c059f805bc1cc183837864c404d349d4f3800f6e
“shwetabh-23” <shwetabhbiswas2305@gmail.com>
2023-06-02 17:57:25 +0530

    Stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -15,6 +15,7 @@ def get_data(config_path):
   data_path = config['data_source']['s3_source']
   df = pd.read_csv(data_path, sep = ',', encoding='utf-8')
   return df
+
 if __name__=='__main__':
   args = argparse.ArgumentParser()
   args.add_argument('--config', default='params.yaml')
